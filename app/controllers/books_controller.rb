ed9b655d2453f66f7b7fe5f525dc35953d106050
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-10-05 08:21:03 +0000

    [rest]5-9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,9 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @book_comment = BookComment.new
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
   end
 
   def index
@@ -16,6 +19,10 @@ class BooksController < ApplicationController
         a.favorited_users.includes(:favorites).where(created_at: from...to).size
       }
     @book = Book.new
+    @book_detail = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.view_counts.create(book_id: @book_detail.id)
+    end
   end
 
   def create
